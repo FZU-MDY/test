074097408422eaa768271393454229117ffc7ad8
77nuyoah <90207732+77nuyoah@users.noreply.github.com>
2021-10-30 09:56:40 +0800

    Create random.cpp

---
+++ random.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<stdlib.h>
+#include<ctime>
+using namespace std;
+string result;
+int Rand100(){				//生成0-100整型的随机数 
+}
+int Int_Rand10000(){		//生成0-10000整型的随机数 
+}
+double Double_Rand10000(){	//生成0-10000浮点型的随机数 
+}
+int  Equation_Number(){
+}
+int Add_Sub_Choose(){
+}
+int Add_Sub_Multi_Divid_Choose(){
+}
+string Transform_Int_String(int a){
+}
+string Transform_Double_String(double a){
+}
+string Connect_eqution_low(){
+}
